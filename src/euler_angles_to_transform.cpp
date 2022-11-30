ac6898eff47e84c04237aa82756ed2306d501a40
Winnerrang <leo.hanxu@mail.utoronto.ca>
2022-11-30 18:06:53 -0500

    merging

--- src/euler_angles_to_transform.cpp
+++ src/euler_angles_to_transform.cpp
@@ -14,8 +14,6 @@ Eigen::Affine3d euler_angles_to_transform(
   float degToRad = M_PI / 180;
 
 
-  A.matrix().setIdentity();
-  R.setIdentity();
   R = Eigen::AngleAxisd(xzx[2] * degToRad, Eigen::Vector3d::UnitX()) *
       Eigen::AngleAxisd(xzx[1] * degToRad, Eigen::Vector3d::UnitZ()) *
       Eigen::AngleAxisd(xzx[0] * degToRad, Eigen::Vector3d::UnitX());
@@ -23,7 +21,7 @@ Eigen::Affine3d euler_angles_to_transform(
 ;
   A.matrix() << R, Eigen::Vector3d::Zero(),
                 0, 0, 0, 1;
-  std::cout << A.matrix() << std::endl;
+ 
   return A;
   /////////////////////////////////////////////////////////////////////////////
 }
