596b1869fdd762810a54f91a1e2e3ee7522a2a45
Winnerrang <leo.hanxu@mail.utoronto.ca>
2022-11-30 14:28:05 -0500

    Finish euler angles to transform function and debugging it

--- src/euler_angles_to_transform.cpp
+++ src/euler_angles_to_transform.cpp
@@ -1,4 +1,7 @@
 #include "euler_angles_to_transform.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <iostream>
 
 Eigen::Affine3d euler_angles_to_transform(
   const Eigen::Vector3d & xzx)
@@ -6,11 +9,21 @@ Eigen::Affine3d euler_angles_to_transform(
   /////////////////////////////////////////////////////////////////////////////
   // Replace with your code
   Eigen::Affine3d A;
-  A.matrix() << 
-    1,0,0,0,
-    0,1,0,0,
-    0,0,1,0,
-    0,0,0,1;
+  Eigen::Matrix3d R;
+
+  float degToRad = M_PI / 180;
+
+
+  A.matrix().setIdentity();
+  R.setIdentity();
+  R = Eigen::AngleAxisd(xzx[2] * degToRad, Eigen::Vector3d::UnitX()) *
+      Eigen::AngleAxisd(xzx[1] * degToRad, Eigen::Vector3d::UnitZ()) *
+      Eigen::AngleAxisd(xzx[0] * degToRad, Eigen::Vector3d::UnitX());
+  
+;
+  A.matrix() << R, Eigen::Vector3d::Zero(),
+                0, 0, 0, 1;
+  std::cout << A.matrix() << std::endl;
   return A;
   /////////////////////////////////////////////////////////////////////////////
 }
