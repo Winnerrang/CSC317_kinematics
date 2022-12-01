382759649f1e30b08c9fa1ab74227160fb7f1a1e
Winnerrang <leo.hanxu@mail.utoronto.ca>
2022-12-01 10:42:12 -0500

    finish transformed_tips.cpp but have not debug yet

--- src/transformed_tips.cpp
+++ src/transformed_tips.cpp
@@ -1,5 +1,7 @@
+
 #include "transformed_tips.h"
 #include "forward_kinematics.h"
+#include <iostream>
 
 Eigen::VectorXd transformed_tips(
   const Skeleton & skeleton, 
@@ -7,6 +9,18 @@ Eigen::VectorXd transformed_tips(
 {
   /////////////////////////////////////////////////////////////////////////////
   // Replace with your code
-  return Eigen::VectorXd::Zero(3*b.size());
+	std::vector<Eigen::Affine3d, Eigen::aligned_allocator<Eigen::Affine3d> > T;
+	Eigen::VectorXd tip_positions = Eigen::VectorXd::Zero(3 * b.size());
+	forward_kinematics(skeleton, T);
+	
+	for (int i = 0; i < b.size(); i++) {
+		auto tip = T[b(i)] * Eigen::Vector3d(skeleton[b(i)].length, 0, 0);
+		
+		for (int j = 0; j < 3; j++) {
+			tip_positions(3 * i + j) = tip(j);
+		}
+	}
+
+	return tip_positions;
   /////////////////////////////////////////////////////////////////////////////
 }
