a8f7abe85ec10310ae07370b29c45d49c8869b9b
Winnerrang <leo.hanxu@mail.utoronto.ca>
2022-12-01 20:37:29 -0500

    finish copy skeleton

--- src/copy_skeleton_at.cpp
+++ src/copy_skeleton_at.cpp
@@ -1,11 +1,17 @@
 #include "copy_skeleton_at.h"
+#include <iostream>
+
 Skeleton copy_skeleton_at(
   const Skeleton & skeleton, 
   const Eigen::VectorXd & A)
 {
   /////////////////////////////////////////////////////////////////////////////
   // Replace with your code
-  Skeleton copy = skeleton;
-  return copy;
+	Skeleton copy = skeleton;
+	
+	for (int bone_index = 0; bone_index < skeleton.size(); bone_index++) {
+		copy[bone_index].xzx = Eigen::Vector3d(A(bone_index * 3), A(bone_index * 3 + 1), A(bone_index * 3 + 2));
+	}
+	return copy;
   /////////////////////////////////////////////////////////////////////////////
 }
